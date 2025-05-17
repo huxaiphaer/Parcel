48da6a67bce17e58107c98b1bdeef4efe9178696
Huzaifah <huzaifah.lutaaya@lawpilots.com>
2025-05-17 11:29:38 +0200

    Add Shipment, Article models

--- Parcels/settings.py
+++ Parcels/settings.py
@@ -9,21 +9,25 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+load_dotenv()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-1+w1v!37_w8)=er@j830mufl+e9)z4%@imk=kz4n)lq+#muz2!"
+SECRET_KEY = os.getenv('SECRET_KEY', 'test')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'True').lower() == 'true'
+
+
 
 ALLOWED_HOSTS = []
 
@@ -37,6 +41,13 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    # Third-party apps
+    "django_extensions",
+
+    # Local apps
+    "shipments",
+    "weather",
 ]
 
 MIDDLEWARE = [
--- shipments/models.py
+++ shipments/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django_softdelete.models import SoftDeleteModel
+import uuid as uuid
+from django_extensions.db.models import TimeStampedModel
 
-# Create your models here.
+
+class Shipment(TimeStampedModel, SoftDeleteModel):
+    uuid = models.UUIDField(
+        unique=True,
+        max_length=500,
+        default=uuid.uuid4,
+        editable=False,
+        db_index=True,
+        blank=False,
+        null=False,
+    )
+    tracking_number = models.CharField(max_length=50)
+    carrier = models.CharField(max_length=50)
+    sender_address = models.TextField()
+    receiver_address = models.TextField()
+    status = models.CharField(max_length=50)
+
+
+class Article(TimeStampedModel, SoftDeleteModel):
+    uuid = models.UUIDField(
+        unique=True,
+        max_length=500,
+        default=uuid.uuid4,
+        editable=False,
+        db_index=True,
+        blank=False,
+        null=False,
+    )
+    shipment = models.ForeignKey(Shipment, related_name='articles', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    sku = models.CharField(max_length=50)
